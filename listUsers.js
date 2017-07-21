9dc53cbf3a5e4b8dba489c71c0c7359a681d8d42
Satyam Jain <satyam.jain@tcs.com>
2017-07-21 15:17:11 +0530

    Port code commit change

--- listUsers.js
+++ listUsers.js
@@ -113,7 +113,7 @@ app.post('/getPoemDetails', function (req, res) {
             }, 1500);
    });
 })
-var server = app.listen(8081, '0.0.0.0', function () {
+var server = app.listen(process.env.PORT || 8081, '0.0.0.0', function () {
 
   var host = server.address().address
   var port = server.address().port
